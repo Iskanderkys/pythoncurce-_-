53385c1adc42ae07dd289d9b978019eaef0b6226
Iskander <iskanderkys@gmail.com>
2019-10-03 20:22:34 +0300

    tree

---
+++ range_feature.py
@@ -0,0 +1,7 @@
+height = int(input("Enter height> "))
+for i in range(height):
+    for z in range(height - i):
+        print(" ", end="")
+    for k in range(i):
+        print("##", end="")
+    print()
